8f55928918ebf201e5d473d6054d83e9ce0f4da2
Sergiy <s.pometun@samsung.com>
2017-05-01 22:12:14 +0300

    first version of automated templates and www generation done

--- script/libs/generate_language.py
+++ script/libs/generate_language.py
@@ -7,26 +7,26 @@ from libs.pipeline_classes import *
 from libs.utils import *
 
 def generate_language(language):
-    lang_root = ROOT + LANG
+    print("GENERATING LANGUAGE [" + language + "]")
     src_studies = ROOT + TEMPLATE + WWW + STUDIES + ENGLISH
     dst_studies = ROOT + WWW + STUDIES
 
-    po = polib.pofile(lang_root + "/" + language + "/LC_MESSAGES/" + FP_DOMAIN + ".po")
-    po.save_as_mofile(lang_root + "/" + language + "/LC_MESSAGES/" + FP_DOMAIN + ".mo")
+    po = polib.pofile(ROOT + LANG + "/" + language + "/LC_MESSAGES/" + FP_DOMAIN + ".po")
+    po.save_as_mofile(ROOT + LANG + "/" + language + "/LC_MESSAGES/" + FP_DOMAIN + ".mo")
 
-    #gettext.find(FP_DOMAIN, lang_root, language)
-    translation = gettext.translation(FP_DOMAIN, localedir=lang_root, languages=[language])
-    print(translation.gettext("God knows your needs"))
-    print(translation.gettext("Hello World"))
+    #gettext.find(FP_DOMAIN, ROOT + LANG, language)
+    translation = gettext.translation(FP_DOMAIN, localedir = ROOT + LANG, languages=[language])
     recreate_dir(dst_studies + "/" + language)
-    
-    input_file = open(src_studies + "/seeking_god.html")
-    text = input_file.read()
-    parser = ParserSource(text)
-    out_file = open(dst_studies + "/" + language + "/" + "/seeking_god.html", "w")
-    writer = WriterSink(out_file)
-    translator = TranslatorSinkSource()
-    translator.language = translation
-    parser.sink = translator
-    translator.sink = writer
-    parser.go()
+
+    for study in STUDY_LIST:
+        print(study.name)
+        with open(src_studies + "/" + study.name + ".html") as input_file:
+            text = input_file.read()
+        parser = ParserSource(text)
+        with open(dst_studies + "/" + language + "/" + study.name + ".html", "w") as out_file:
+            writer = WriterSink(out_file)
+            translator = TranslatorSinkSource()
+            translator.language = translation
+            parser.sink = translator
+            translator.sink = writer
+            parser.go()
