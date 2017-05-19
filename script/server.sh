5a08cb5a5d77aeb6cdbb14fd202d16eaa0f10884
Vlad Dziuba <dzyubavlad@gmail.com>
2017-05-19 17:30:03 +0300

    Script for starting simple server
    It's needed for relative links in web-pages

---
+++ script/server.sh
@@ -0,0 +1,10 @@
+cd ../../
+URL=http://localhost:8000/www/studies/en/
+if which xdg-open > /dev/null
+then
+  xdg-open $URL
+elif which gnome-open > /dev/null
+then
+  gnome-open $URL
+fi
+python -m SimpleHTTPServer
