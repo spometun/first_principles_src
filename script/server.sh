d62c6f8d2ddec5be6ba58290a6b9c4204ddd54eb
Vlad Dziuba <dzyubavlad@gmail.com>
2017-05-20 00:23:44 +0300

    Impove server script

--- script/server.sh
+++ script/server.sh
@@ -2,9 +2,9 @@ cd ../../
 URL=http://localhost:8000/www/studies/en/
 if which xdg-open > /dev/null
 then
-  xdg-open $URL
+  xdg-open $URL &
 elif which gnome-open > /dev/null
 then
-  gnome-open $URL
+  gnome-open $URL &
 fi
 python -m SimpleHTTPServer
