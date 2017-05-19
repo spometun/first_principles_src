f8b347599fe9b2b55a6d5221b949517b4fb023e6
Vlad Dziuba <dzyubavlad@gmail.com>
2017-05-19 16:03:54 +0300

    Move windy.py to script folder

--- script/windy.py
+++ script/windy.py
@@ -5,11 +5,13 @@
 
 import os
 
+ROOT = ".."
+
 def listdir_html(dirname):
     return set([f for f in os.listdir(dirname) if f.endswith(".html")])
 
-old_www_files = listdir_html("old_www")
-www_files = listdir_html("www/studies/english")
+old_www_files = listdir_html(os.path.join(ROOT, "old_www"))
+www_files = listdir_html(os.path.join(ROOT, "www/studies/english"))
 
 for f in old_www_files - www_files:
     print(f)
