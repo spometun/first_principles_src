f24d0caa0c8f84a2487e03ba0cb37079aee20c00
Sergiy Pometun <s.pometun@samsung.com>
2017-05-23 02:36:58 +0300

    + pot repeated terms check

--- script/0_generate_template.py
+++ script/0_generate_template.py
@@ -15,31 +15,8 @@ def prepare_dst_folder():
     copy_fixed_stuff(ROOT + SRC + WWW, dst_path + WWW)
     os.mkdir(dst_path + WWW + STUDIES)
     os.mkdir(dst_path + WWW + STUDIES + ENGLISH)
-    global g_dst_studies 
-    g_dst_studies = dst_path + WWW + STUDIES + ENGLISH
     os.mkdir(dst_path + LANG)
     os.mkdir(dst_path + LANG + ENGLISH_TEMPLATE)
-    global g_dst_english_template
-    g_dst_english_template = dst_path + LANG + ENGLISH_TEMPLATE
-
-def cut_number(filename):
-    return filename.split("_", 1)[1];
-
-def generate_po(path_to_studies, output_folder, study_name):
-    input_file = open(path_to_studies + "/" + study_name + ".html")
-    input_text = input_file.read()
-    out_file_name = output_folder + "/" + cut_number(study_name) + ".pot"
-    out_file = open(out_file_name, "w")
-    writer = WriterSink(out_file)
-    parser = ParserSource(input_text)
-    po_generator = POGeneratorSinkSource(study_name + ".html")
-    parser.sink = po_generator
-    po_generator.sink = writer
-    parser.go()
-    out_file.close()
-    po = polib.pofile(out_file_name)
-    po.save(out_file_name + "2")
-
 
 def generate_studies_templates():
     print("GENERATING STUDIES TEMPLATES")
@@ -50,6 +27,36 @@ def generate_studies_templates():
         generateStudy(src, dst, study)    
 
 
+def cut_front_number(filename):
+    return filename.split("_", 1)[1];
+
+
+def generate_po(path_to_studies, output_folder, study_name):
+    input_file = open(path_to_studies + "/" + study_name + ".html")
+    input_text = input_file.read()
+    out_file_name = output_folder + "/" + cut_front_number(study_name) + ".pot"
+    out_file = open(out_file_name, "w")
+    writer = WriterSink(out_file)
+    parser = ParserSource(input_text)
+    po_generator = POGeneratorSinkSource(study_name + ".html")
+    parser.sink = po_generator
+    po_generator.sink = writer
+    parser.go()
+
+
+def post_process_po(output_folder, study_name):
+    global g_terms_dict
+    study_name = cut_front_number(study_name)
+    po = polib.pofile(output_folder + "/" + study_name + ".pot")
+    for entry in po:
+        if entry.msgid not in g_terms_dict:
+            g_terms_dict[entry.msgid] = [study_name]
+        else:
+            g_terms_dict[entry.msgid].append(study_name)
+
+ #   po.save(out_file_name + "2")
+
+
 def generate_translation_templates():
     print("\nGENERATING TRANSLATION TEMPLATES")
     path_to_studies = ROOT + TEMPLATE + WWW + STUDIES + ENGLISH
@@ -57,10 +64,18 @@ def generate_translation_templates():
     for study in STUDY_LIST:
         print(study.name)
         generate_po(path_to_studies, output_folder, study.name)
+        post_process_po(output_folder, study.name)
+    print("\n\nREPEATED TERMS (except index):\n")
+    for key in g_terms_dict:
+        studies = g_terms_dict[key]
+        if(len(studies) >= 2 and studies.count("index") == 0):
+            print(key, "    ", studies)
 
+g_terms_dict = {};  
 prepare_dst_folder()
 generate_studies_templates()
 generate_translation_templates()
+
 
 print("\n" + str(len(STUDY_LIST)) + " studies processed")
 
