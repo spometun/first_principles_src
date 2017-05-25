6226fbfbaa9b4e4b00708c5295a1f16cdd9f12c8
Vlad Dziuba <dzyubavlad@gmail.com>
2017-05-25 11:59:54 +0300

    Implement github pulling and script executing

--- script/server.py
+++ script/server.py
@@ -3,10 +3,24 @@
 from http.server import SimpleHTTPRequestHandler, BaseHTTPRequestHandler
 import subprocess
 import socketserver
+import os
 
 PORT = 8000
 
 PATH_FOR_GITHUB_COMMIT = '/api/new-commit'
+
+SCRIPT_DIR = os.path.abspath(os.curdir)
+ROOT_DIR = os.path.join(SCRIPT_DIR, '..', '..')
+WWW_DIR = os.path.join(ROOT_DIR, 'www')
+LANG_DIR = os.path.join(ROOT_DIR, 'lang')
+
+os.chdir(WWW_DIR)
+
+def update_pages():
+    os.chdir(LANG_DIR)
+    subprocess.call(["git", "pull"])
+    os.chdir(SCRIPT_DIR)
+    subprocess.call(["sh", "run_all.sh"])
 
 class AppRequestHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -18,13 +32,11 @@ class AppRequestHandler(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write('Hello, world!'.encode('utf-8'))
-            print(subprocess.check_output(["git", "pull"]))
+            update_pages()
         else:
             SimpleHTTPRequestHandler.do_GET(self, *args, **kwargs)
 
-Handler = AppRequestHandler
-
-httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd = socketserver.TCPServer(('', PORT), AppRequestHandler)
 
 print("Serving at port", PORT)
 httpd.serve_forever()
