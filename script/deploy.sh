0242804648f881784c907166e3b914cd21248016
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-02 05:56:57 +0300

    Add check for directory existing

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,10 @@ if [ "$#" != 1 ]; then
   echo "Usage: $0 <destination_path>"
   exit 1
 fi
+if [ ! -d $1 ]; then
+  echo "$1 does not exist"
+  exit 1
+fi
 ./build.sh $1
 ./2_generate_translations.py $1
 ./copy_server.sh $1
