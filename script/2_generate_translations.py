cd78f40705d68d2161b83fab1d5a6cd780613e7e
Sergiy Pometun <sergiy.pometun@usd21.org>
2017-06-02 06:08:03 +0300

    start adding mobile

--- script/2_generate_translations.py
+++ script/2_generate_translations.py
@@ -10,7 +10,7 @@ from libs.generate_language import *
 if len(sys.argv) != 2:
     print('Usage: {} <destination_path>'.format(sys.argv[0]))
     sys.exit()
-DST_FOLDER = sys.argv[1]
+DEPLOYMENT_FOLDER = sys.argv[1]
 
 recreate_dir(DST_FOLDER + WWW);
 copy_fixed_stuff(DST_FOLDER + TEMPLATE + WWW, DST_FOLDER + WWW)
