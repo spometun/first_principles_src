7a64bf29ea41f91236daf6c471303af3b8778866
Sergiy Pometun <sergiy.pometun@usd21.org>
2017-06-02 05:31:50 +0300

    minor fixes

--- script/1_copy_english_template_to_lang.py
+++ script/1_copy_english_template_to_lang.py
@@ -6,11 +6,11 @@ from context import *
 from libs.utils import *
 
 if len(sys.argv) != 2:
-    print('Usage: {} <destination_path>'.format(sys.argv[0]))
+    print('Usage: {} <deployment_path>'.format(sys.argv[0]))
     sys.exit()
-DST_FOLDER = sys.argv[1]
+DEPLOYMENT_FOLDER = sys.argv[1]
 
 dst_path = ROOT + LANG +  ENGLISH_TEMPLATE
 if os.path.exists(dst_path):
         shutil.rmtree(dst_path)
-shutil.copytree(DST_FOLDER + TEMPLATE + LANG + ENGLISH_TEMPLATE, dst_path)
+shutil.copytree(DEPLOYMENT_FOLDER + TEMPLATE + LANG + ENGLISH_TEMPLATE, dst_path)
