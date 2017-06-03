24a89cae387b4f624a66b06ccf3671c67d7f0bc6
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-03 22:57:18 +0300

    fixup! Add logging for php scripts

--- server/update_translations.php
+++ server/update_translations.php
@@ -7,7 +7,7 @@ echo "Hello God!";
 $ch = curl_init("https://poeditor.com/api/webhooks/github?api_token=5b0d77bc255634323a31af2df41c1388&id_project=106095&language=uk&operation=export_terms_and_translations");
 curl_exec($ch);
 
-file_put_contents('logs.txt', date("Y-m-d|h:m:s: ")."Call to poeditor webhook\n" , FILE_APPEND);
+file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Call to poeditor webhook\n" , FILE_APPEND);
 ?>
 
 </body>
