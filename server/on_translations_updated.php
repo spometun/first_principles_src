447a918dede5f26878f254edc2be2ad4b00c5b55
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-03 22:42:05 +0300

    Add logging for php scripts

--- server/on_translations_updated.php
+++ server/on_translations_updated.php
@@ -4,9 +4,10 @@
 
 <?php
 echo "Hello God! Updated";
-$output = shell_exec('date | cat >> updates.txt');
 $src_path = file_get_contents('src_path.txt');
 `cd {$src_path}; cd ../../lang; git pull; cd ../src/script; ./2_generate_translations.py`;
+
+file_put_contents('logs.txt', date("Y-m-d|h:m:s: ")."Language repository update" , FILE_APPEND);
 ?>
 
 </body>
