e1eb28140b71adf3a468ae59ba8221c3fac7a233
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-03 23:30:04 +0300

    fixup! Fix lang repo updating

--- server/on_translations_updated.php
+++ server/on_translations_updated.php
@@ -5,7 +5,8 @@
 <?php
 echo "Hello God! Updated";
 $src_path = file_get_contents('src_path.txt');
-`cd {$src_path}; cd ../../lang; git pull; cd ../src/script; ./2_generate_translations.py`;
+// src_path has line end character so semicolon isn't needed
+`cd {$src_path} cd ../../lang; git pull; cd ../src/script; ./2_generate_translations.py`;
 
 file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Language repository update\n" , FILE_APPEND);
 ?>
