cb746646c3e2971270908093d33d283d0b206010
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-03 23:47:23 +0300

    fixup! Add build and deploy scripts

--- server/on_translations_updated.php
+++ server/on_translations_updated.php
@@ -6,7 +6,7 @@
 echo "Hello God! Updated";
 $src_path = file_get_contents('src_path.txt');
 // src_path has line end character so semicolon isn't needed
-`cd {$src_path} cd ../../lang; git pull; cd ../src/script; ./2_generate_translations.py`;
+`root_dir=$(pwd)/../; cd {$src_path} cd ../../lang; git pull; cd ../src/script; ./2_generate_translations.py \$root_dir`;
 
 file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Language repository update\n" , FILE_APPEND);
 ?>
