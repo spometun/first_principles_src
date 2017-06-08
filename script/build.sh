683002c8eb6fe86874275f69cbc80703856817c8
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-08 20:33:46 +0300

    Add flag for hiding refresh button

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-if [ "$#" != 1 ]; then
-  echo "Usage: $0 <destination_path>"
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ] ; then
+  echo "Usage: $0 <destination_path> [--mobile]"
   exit 1
 fi
-./0_generate_template.py $1
+./0_generate_template.py $1 $2
 ./1_copy_english_template_to_lang.py $1
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
-if [ "$#" != 1 ]; then
-  echo "Usage: $0 <destination_path>"
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ] ; then
+  echo "Usage: $0 <destination_path> [--mobile]"
   exit 1
 fi
 if [ ! -d $1 ]; then
   echo "$1 does not exist"
   exit 1
 fi
-./build.sh $1
+./build.sh $1 $2
 ./2_generate_translations.py $1
 ./copy_server.sh $1
