9737416fbc23646894a727d0340fbc3ce09d71f7
Sergiy Pometun <sergiy.pometun@usd21.org>
2017-06-08 22:27:34 +0300

    bux fix

--- script/2_generate_translations.py
+++ script/2_generate_translations.py
@@ -10,17 +10,18 @@ from libs.generate_language import *
 if len(sys.argv) != 2:
     print('Usage: {} <destination_path>'.format(sys.argv[0]))
     sys.exit()
+
 DEPLOYMENT_FOLDER = sys.argv[1]
 
-recreate_dir(DST_FOLDER + WWW);
-copy_fixed_stuff(DST_FOLDER + TEMPLATE + WWW, DST_FOLDER + WWW)
-shutil.copy(DST_FOLDER + TEMPLATE + WWW + "/languages.json", DST_FOLDER + WWW)
-os.mkdir(DST_FOLDER + WWW + STUDIES)
+recreate_dir(DEPLOYMENT_FOLDER + WWW);
+copy_fixed_stuff(DEPLOYMENT_FOLDER + TEMPLATE + WWW, DEPLOYMENT_FOLDER + WWW)
+shutil.copy(DEPLOYMENT_FOLDER + TEMPLATE + WWW + "/languages.json", DEPLOYMENT_FOLDER + WWW)
+os.mkdir(DEPLOYMENT_FOLDER + WWW + STUDIES)
 
-generate_language("en", DST_FOLDER)
-generate_language("ru", DST_FOLDER)
-generate_language("uk", DST_FOLDER)
-generate_language("sv", DST_FOLDER)
+generate_language("en", DEPLOYMENT_FOLDER)
+generate_language("ru", DEPLOYMENT_FOLDER)
+generate_language("uk", DEPLOYMENT_FOLDER)
+generate_language("sv", DEPLOYMENT_FOLDER)
 
 
 with open('logs.txt', 'a') as f:
