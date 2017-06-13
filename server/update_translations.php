f4a7d1c9a37840f445b088a1f9b953e319856c4e
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-13 15:59:29 +0300

    fixup! Add update_translations script waiting for pulling new commits

--- server/update_translations.php
+++ server/update_translations.php
@@ -5,12 +5,12 @@
 <?php
 $SLEEP_INTERVAL = 0.5;
 
+$start_waiting_time = time();
 
 echo "Hello God!";
 $ch = curl_init("https://poeditor.com/api/webhooks/github?api_token=5b0d77bc255634323a31af2df41c1388&id_project=106095&language=uk&operation=export_terms_and_translations");
 curl_exec($ch);
 
-$start_waiting_time = time();
 $needUpdate = true;
 while ($needUpdate) {
   $last_update_time = filemtime("last_update");
