2e81ba54a96ef0618942e40706b4bb66daa7c806
Sergiy Pometun <s.pometun@samsung.com>
2017-06-14 06:39:50 +0300

    bux fix

--- server/on_translations_updated.php
+++ server/on_translations_updated.php
@@ -7,7 +7,7 @@
 echo "Hello God! Updated";
 $src_path = file_get_contents('src_path.txt');
 // src_path has line end character so semicolon isn't needed
-`root_dir=$(pwd)/../; cd {$src_path} cd ../../lang; git pull; cd ../src/script; ./2_generate_translations.py \$root_dir`;
+`root_dir=$(pwd)/../; cd {$src_path} cd ../../lang; git pull; cd ../src/script; ./1_generate_translations.py \$root_dir`;
 
 `touch last_update`;
 
