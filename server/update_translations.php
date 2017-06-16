037149126f1d938da9a94222c9167038c52ffc94
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-16 11:45:24 +0300

    Add returning another response if update is too long

--- server/update_translations.php
+++ server/update_translations.php
@@ -1,30 +1,32 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
 $SLEEP_INTERVAL = 0.5;
+$MAX_WAITING_TIME = 10;
+$OK_RESPONSE = 'ok';
+$TOO_LONG_ERROR = 'update is too long';
 
 $start_waiting_time = time();
 $LANG = $_GET['lang'];
 
-echo "Hello God!";
 file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Triggering update from poeditor for language ".$LANG."\n" , FILE_APPEND);
 $ch = curl_init("https://poeditor.com/api/webhooks/github?api_token=5b0d77bc255634323a31af2df41c1388&id_project=106095&language=".$LANG."&operation=export_terms_and_translations");
 curl_exec($ch);
 
-$needUpdate = true;
-while ($needUpdate) {
-  $last_update_time = filemtime("last_update");
-  if ($last_update_time > $start_waiting_time) {
-    break;
-  }
+$was_update = filemtime("last_update") > $start_waiting_time;
+$too_long = time() - $start_waiting_time > $MAX_WAITING_TIME;
+while (!$was_update && !$too_long) {
   sleep($SLEEP_INTERVAL);
   clearstatcache();
+  $was_update = filemtime("last_update") > $start_waiting_time;
+  $too_long = time() - $start_waiting_time > $MAX_WAITING_TIME;
 }
 
-file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Update detected (lang = ".$LANG.")\n" , FILE_APPEND);
-?>
+if ($was_update) {
+  file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Update detected (lang = ".$LANG.")\n" , FILE_APPEND);
+  echo 'ok';
+}
 
-</body>
-</html>
+if ($too_long) {
+  file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Update is too long (lang = ".$LANG.")\n" , FILE_APPEND);
+  echo 'update is too long';
+}
+?>
