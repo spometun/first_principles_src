f56c50af62dc9eddc3dfeb9f630907e1386423a6
Sergiy Pometun <s.pometun@samsung.com>
2017-06-20 23:51:24 +0300

    better text messages

--- server/on_translations_updated.php
+++ server/on_translations_updated.php
@@ -5,13 +5,14 @@
 <?php
 
 echo "Hello God! Updated";
+file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."New translation update detected. Start updating htmls with new translations\n" , FILE_APPEND);
 $src_path = file_get_contents('src_path.txt');
 // src_path has line end character so semicolon isn't needed
 `root_dir=$(pwd)/../; cd {$src_path} cd ../../lang; git pull; cd ../src/script; ./2_update_translations.py \$root_dir`;
 
 `touch last_update`;
 
-file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Language repository update\n" , FILE_APPEND);
+file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."htmls updated\n" , FILE_APPEND);
 ?>
 
 </body>
