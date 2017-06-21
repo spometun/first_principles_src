296c0c08d61db5544c57a050cbdf2fff4cb1e3a4
Sergiy Pometun <s.pometun@samsung.com>
2017-06-21 05:51:21 +0300

    again better log messages

--- server/on_translations_updated.php
+++ server/on_translations_updated.php
@@ -5,13 +5,15 @@
 <?php
 
 echo "Hello God! Updated";
+
 $src_path = file_get_contents('src_path.txt');
 // src_path has line end character so semicolon isn't needed
-`root_dir=$(pwd)/../; cd {$src_path} cd ../../lang; git pull; cd ../src/script; ./2_update_translation.py \$root_dir web all`;
-
+file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."pulling translation update from github\n" , FILE_APPEND);
+`cd {$src_path} cd ../../lang; git pull`;
+file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Generating htmls\n" , FILE_APPEND);
+`root_dir=$(pwd)/../; cd {$src_path} ./2_update_translation.py \$root_dir web all`;
+file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."htmls generated\n" , FILE_APPEND);
 `touch last_update`;
-
-file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Language repository update\n" , FILE_APPEND);
 ?>
 
 </body>
--- server/update_translations.php
+++ server/update_translations.php
@@ -7,7 +7,7 @@ $TOO_LONG_ERROR = 'update is too long';
 $start_waiting_time = time();
 $LANG = $_GET['lang'];
 
-file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Triggering update from poeditor for language ".$LANG."\n" , FILE_APPEND);
+file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Triggering translation update from poeditor (lang = ".$LANG.")\n" , FILE_APPEND);
 $ch = curl_init("https://poeditor.com/api/webhooks/github?api_token=5b0d77bc255634323a31af2df41c1388&id_project=106095&language=".$LANG."&operation=export_terms_and_translations");
 curl_exec($ch);
 
@@ -21,12 +21,12 @@ while (!$was_update && !$too_long) {
 }
 
 if ($was_update) {
-  file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Update detected (lang = ".$LANG.")\n" , FILE_APPEND);
+  file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Translation update detected (lang = ".$LANG.")\n" , FILE_APPEND);
   echo 'ok';
 }
 
 if ($too_long) {
-  file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Update is too long (lang = ".$LANG.")\n" , FILE_APPEND);
+  file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Update timeout (lang = ".$LANG.")\n" , FILE_APPEND);
   echo 'update is too long';
 }
 ?>
