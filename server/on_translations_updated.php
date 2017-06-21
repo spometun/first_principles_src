5e74dc46bbd1035a53375b0d83db177af08a8081
Sergiy Pometun <s.pometun@samsung.com>
2017-06-21 06:05:33 +0300

    fix

--- server/on_translations_updated.php
+++ server/on_translations_updated.php
@@ -9,7 +9,7 @@ echo "Hello God! Updated";
 $script_path = file_get_contents('script_path.txt');
 // src_path has line end character so semicolon isn't needed
 file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."pulling translation update from github\n" , FILE_APPEND);
-`cd {$script_path}/../../lang; git pull`;
+`cd {$script_path} cd ../../lang; git pull`;
 file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."Generating htmls\n" , FILE_APPEND);
 `deployment_dir=$(pwd)/../; cd {$script_path} ./2_update_translation.py \$deployment_dir web all`;
 file_put_contents('logs.txt', date("Y-m-d|H:i:s: ")."htmls generated\n" , FILE_APPEND);
