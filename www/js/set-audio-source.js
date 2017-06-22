e09239b2422373d17f698711cd0c00583535fff7
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-22 09:59:51 +0300

    implement multiformat audio

--- www/js/set-audio-source.js
+++ www/js/set-audio-source.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', () => {
   const BASE_URL = '../../../audio';
   const audio = document.getElementById('audio');
-  const audioSource = document.getElementById('audio_link_mp3');  
-  audioSource.src = [
-    BASE_URL, '/', CUR_LANGUAGE.language, '/', PAGE_NAME, '.mp3'
-  ].join('');
+  const audioExts = ['m4a', 'ogg', 'mp3'];
+  for (const ext of audioExts) {
+    const audioSource = document.getElementById('audio_link_' + ext);
+    audioSource.src = [
+      BASE_URL, '/', CUR_LANGUAGE.language, '/', PAGE_NAME, '.', ext
+    ].join('');
+  }
   audio.load();
 });
--- www/js/utils.js
+++ www/js/utils.js
@@ -4,7 +4,6 @@ const { pathname } = window.location;
 const lastSlashIndex = pathname.lastIndexOf('/');
 const lastDotIndex = pathname.lastIndexOf('.');
 const PAGE_NAME = pathname.slice(lastSlashIndex + 1, lastDotIndex);
-console.log(lastSlashIndex, lastDotIndex, PAGE_NAME);
 
 let CUR_LANGUAGE;
 for (var i = 0; i < LANGUAGES.length; i++) {
