cda1b498952b2ca2a7462479f75e7d8a20045a4a
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-25 22:31:59 +0300

    Remove other audio formats different from mp3

--- www/js/set-audio-source.js
+++ www/js/set-audio-source.js
@@ -1,15 +1,10 @@
 window.addEventListener('load', () => {
   const BASE_URL = '../../../audio';
-  const audio = document.getElementById('audio');
 
-  const audioExts = ['m4a', 'ogg', 'mp3'];
-  let errCount = 0;
-  for (const ext of audioExts) {
-    const audioSource = document.getElementById('audio_link_' + ext);
-    audioSource.src = [
-      BASE_URL, '/', CUR_LANGUAGE.language, '/', PAGE_NAME, '.', ext
-    ].join('');
-  }
+  const audio = document.getElementById('audio');
+  audio.src = [
+    BASE_URL, '/', CUR_LANGUAGE.language, '/', PAGE_NAME, '.mp3'
+  ].join('');
 
   audio.addEventListener('loadeddata', () => {
     audio.style.display = '';
